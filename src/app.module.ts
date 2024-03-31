7efce8dd4ff4e5b77fb2a6165501a02282b3596d
Sarfaraj Shahjahan <dee.tech.au@gmail.com>
2024-03-31 23:13:05 +0300

    feat: setup database

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,12 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { ShortUrlController } from './short-url/short-url.controller';
+import { PrismaService } from './prisma/prisma.service';
+import { ShortUrlService } from './short-url/short-url.service';
 
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+  controllers: [AppController, ShortUrlController],
+  providers: [AppService, PrismaService, ShortUrlService],
 })
 export class AppModule {}
